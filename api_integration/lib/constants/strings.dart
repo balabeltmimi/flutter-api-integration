7203be2187430bc7ec5287b7193352b548f68dba
phylynxbelt <thanaporn.jira@odds.team>
2021-05-07 02:38:22 +0700

    feat: add news api and newsInfo model

---
+++ api_integration/lib/constants/strings.dart
@@ -0,0 +1,4 @@
+class Strings {
+  static String newsUrl =
+      'https://newsapi.org/v2/everything?q=apple&from=2021-05-05&to=2021-05-05&sortBy=popularity&apiKey=API_KEY';
+}
---
+++ api_integration/lib/services/api_manager.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:api_integration/constants/strings.dart';
+import 'package:api_integration/models/newsInfo.dart';
+import 'package:http/http.dart' as http;
+
+class APIManager {
+  Future<NewsModel> getNews() async {
+    var client = http.Client();
+    var newsModel = null;
+
+    var response = await client.get(Uri.parse(Strings.newsUrl));
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      var jsonMap = json.decode(jsonString);
+      newsModel = NewsModel.fromJson(jsonMap);
+    }
+    return newsModel;
+  }
+}
